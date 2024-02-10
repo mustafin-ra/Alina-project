60ccefc93cb877389e1e5ceb2015e643dab4b925
Mustafin Renat <96263325+mustafin-ra@users.noreply.github.com>
2024-02-10 08:43:10 +0300

    Add files via upload

---
+++ script_3.2.js
@@ -0,0 +1,31 @@
+const text= document.querySelectorAll('.text-slider');
+const controlls = document.querySelectorAll('.controlls');
+let textIndex = 0;
+
+function show(index) {
+    images[imageIndex].classList.remove('active');
+    images[index].classList.add('active');
+    imageIndex = index;
+}
+
+controlls.forEach((e) => {
+    e.addEventListener('click', () => {
+        if (event.target.classList.contains('prev')) {
+            let index = imageIndex - 1;
+
+            if (index < 0) {
+                index = images.length - 1;
+            }
+
+            show(index);
+        } else if (event.target.classList.contains('next')) {
+            let index = imageIndex + 1;
+            if (index >= images.length) {
+                index = 0;
+            }
+            show(index);
+        }
+    })
+})
+
+show(imageIndex);
